1685aa63f98049071f9ee2badecf7c41f74b01e3
root <root@52jzy.com>
2021-01-21 15:58:37 +0800

    first commit

---
+++ webhook.php
@@ -0,0 +1,8 @@
+<?php
+
+$cmd = "cd /var/www/edu && pwd";
+
+echo shell_exec('php -v') ."<br>";  //用来测试exec命令是否有权限执行
+echo shell_exec('whoami') ."<br>";  //用来查看当前执行者得用户名称
+
+var_dump(shell_exec($cmd)); //返回成功还是null
